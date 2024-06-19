19f910c5f98fe27cefed4dec418d66f662a92bdb
Sandra Andrade <145820828+sammy-regina@users.noreply.github.com>
2024-06-18 21:04:44 -0300

    Inclui contagem crescente e regressiva

--- src/main/java/intro/Medida.java
+++ src/main/java/intro/Medida.java
@@ -19,6 +19,8 @@ public class Medida {
         System.out.println("c - Calcular Area Compacta: ");
         System.out.println("l - Calcular Area Modo Longo: ");
         System.out.println("i - Calcular IfSimples: ");
+        System.out.println("d - Contagem Crescente: ");
+        System.out.println("r - Contagem Regressiva: ");
         System.out.println("Digite a opção desejada: ");
         String opcao = scanner.next();
 
@@ -27,16 +29,29 @@ public class Medida {
 
         switch (opcao){
             case "i":
+            case "I":
                 System.out.println("Você escolheu executar o método ifSimples!");
                 ifSimples();
                 break;
             case "c":
+            case "C":
                 System.out.println("Você escolheu executar o método compacta!");
                 calcularAreaCompacta();
                 break;
             case "l":
+            case "L":
                 System.out.println("Você escolheu executar o método longa!");
                 calcularAreaLonga();
+                break;
+            case "D":
+            case "d":
+                System.out.println("Você escolheu executar o método contarAteDez!");
+                contarAteDez();
+                break;
+            case "R":
+            case "r":
+                System.out.println("Você escolheu executar o método contarAteZero!");
+                contarAteZero();
                 break;
             default:
                 System.out.println("Escolha um valor válido!");
@@ -84,5 +99,21 @@ public class Medida {
 
         System.out.println("Para a largura de " + largura + "m e o comprimento de "
                 + comprimento + "m a área é de " + largura * comprimento + "m².");
+    }
+    public static void contarAteDez(){
+        //Laços de repetições
+        //for = repetição incondicional
+        System.out.println("contagem Crescente");
+        for (int numero = 1; numero <= 10; numero ++){
+            System.out.println(numero);
+        }
+    }
+    public static void contarAteZero(){
+        //Laços de repetições
+        //for = repetição incondicional
+        System.out.println("contagem Regressiva");
+        for (int numero = 10; numero >= 0; numero --){
+            System.out.println(numero);
+        }
     }
 }
